05ac98ca86893c23d81c49da94019f15f4fd05ef
Gamigo <1160667509@qq.com>
2019-03-20 16:06:20 +0800

    The first java file

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(new Date());
+        //System.getProperties().list(System.out);
+        //System.out.println(System.getProperties("user.name"));
+        //System.out.println(System.getProperties("java.library.path"));
+    }
+}
